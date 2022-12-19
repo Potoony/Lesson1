64f9412a2d94f1e8ecbe15072ad64dda09b4a853
potoony <scharypik@gmail.com>
2022-12-19 20:58:47 +0300

    program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Нахождение максимального числа
+
+// Console.Write("Введите число x:");
+// int x = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите число y:");
+// int y = Convert.ToInt32(Console.ReadLine());
+// if (x>y)
+//     {
+//         Console.WriteLine(x);
+//     }
+// else
+//     {
+//         Console.WriteLine(y);
+//     }
+
+// Нахождение максимума среди 3 чисел
+
+Console.Write("Введите число x:");
+int numberx = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число y:");
+int numbery = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число z:");
+int numberz = Convert.ToInt32(Console.ReadLine());
+
+int max = numberx;
+if (numbery>max) max = numbery;
+if (numberz>max) max = numberz;
+Console.Write(max);
+
+// Проверка, является ли число чётным
+
+// Console.Write("Введите: x = ");
+// int x = Convert.ToInt32(Console.ReadLine());
+// if(x%2 == 0)
+// {
+//     Console.WriteLine("Чётное число");
+// }
+// else
+// {
+//     Console.WriteLine("Не чётное число");
+// }
+
+
